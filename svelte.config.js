7a6a38c9acf609f0ed1c4023681d08f8a48ce51e
Ambar Mutha <ambar.ytl@gmail.com>
2021-10-06 14:22:43 +0530

    Add adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import postcssNesting from 'postcss-nesting';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,6 +12,7 @@ const config = {
 		}
 	}),
 	kit: {
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
